b3c3307ea2db948c432be647156fad454412b760
jyun9504 <jyun9504@gmail.com>
2019-11-02 16:44:38 +0800

    update

---
+++ src/components/PostItNote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostItNote extends React.Component {
+    render() {
+        return (
+           <div className="postItNote">
+        
+           </div>
+        );
+    }
+}
+
+export default PostItNote;
